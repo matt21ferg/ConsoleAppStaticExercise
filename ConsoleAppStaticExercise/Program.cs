502d03a016d15d4b802f41b8abbaaf09fadf4b6c
matt21ferg <matt21ferguson@gmail.com>
2022-01-24 14:24:24 -0600

    1st commit

---
+++ ConsoleAppStaticExercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleAppStaticExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var celsius = TempConverter.FahrenheitToCelsius(69);
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(25);
+
+
+            Console.WriteLine($"celsius {celsius}");
+            Console.WriteLine($"fahrenheit {fahrenheit}");
+
+
+
+        }
+    }
+}
---
+++ ConsoleAppStaticExercise/TempConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppStaticExercise
+{
+   public static class TempConverter
+    {
+
+        static TempConverter()
+        {
+
+        }
+
+         public static  double FahrenheitToCelsius(double Fahtrenheit)
+        {
+            var result = (Fahtrenheit - 32) / 1.8;
+            return result;
+        }
+
+      public  static double CelsiusToFahrenheit(double Celsius)
+        {
+            return (Celsius * 9) / 5 + 32;
+        }
+
+
+
+    }
+}
